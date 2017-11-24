f980bfdc31a361f961437f092adf78882958789a
Yaniv Hadad <yaniv@similartech.com>
2017-11-24 23:16:06 +0200

    first commit

---
+++ init.sh
@@ -0,0 +1,4 @@
+alias ls='ls --color'
+alias ll='ls -la'
+alias update-all='apt-get update ; apt-get upgrade -y ; apt-get dist-upgrade -y'
+alias install-wifi-adapter='apt-get install -y firmware-realtek'
