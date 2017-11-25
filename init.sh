c29633cb3aae29c2d0538fd534e3151bbbb4e579
Yaniv Hadad <yaniv@similartech.com>
2017-11-25 16:59:41 +0200

    expand partition

--- init.sh
+++ init.sh
@@ -1,4 +1,15 @@
 alias ls='ls --color'
 alias ll='ls -la'
 alias update-all='apt-get update ; apt-get upgrade -y ; apt-get dist-upgrade -y'
-alias install-wifi-adapter='apt-get install -y firmware-realtek'
+
+easy_for_me_base_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function easy-for-me-init() {
+    update-all
+    apt-get install -y curl firmware-realtek vim git
+    chmod +x ${easy_for_me_base_script_dir}/install-raspi-config.sh
+    ${easy_for_me_base_script_dir}/install-raspi-config.sh
+    raspi-config --expand-rootfs
+
+    reboot
+}
